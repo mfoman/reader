d2ec1581d374f1197dfee84200a0a5f94a38d32b
Frederik <mfoman@users.noreply.github.com>
2017-07-02 22:37:47 +0200

    Update my_footer.js

--- includes/my_footer.js
+++ includes/my_footer.js
@@ -1,3 +1,15 @@
 (function () {
-  alert("hello");
+  var search = location.search ? location.search : false;
+  
+  var refer = window.referrer ? window.referrer : false;
+  
+  if (refer != false) {
+  }
+  
+  var content = document.getElementById("reader-content");
+  
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", url, false);
+  xmlHttp.send(null);
+  return xmlHttp.responseText;
 })();
